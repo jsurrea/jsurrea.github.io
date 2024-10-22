3712ed705df6c9e4baa06b535467e10b1f5be4a3
jsurrea <jsurrea@users.noreply.github.com>
2024-10-22 01:09:47 +0000

    deploy: b1d66a37331eaea7b60139dce0c453206575c9fa

---
+++ static/js/660.cf6320c4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_jsurrea=self.webpackChunkportfolio_jsurrea||[]).push([[660],{7660:(e,r,s)=>{s.r(r),s.d(r,{default:()=>a});s(5043),s(4050);var t=s(579);const a=function(e){const{title:r}=e;return(0,t.jsx)("div",{className:"header",children:r})}}}]);
+//# sourceMappingURL=660.cf6320c4.chunk.js.map
