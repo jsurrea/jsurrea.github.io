796f74252f8025869289e99a2510984d29395373
Juan Sebastian Urrea <jurrea5518@gmail.com>
2026-03-08 13:37:39 -0500

    manual_changes

--- src/content/config.ts
+++
@@ -1,4 +0,0 @@
-// Content collections are no longer used.
-// Profile data is fetched dynamically from https://jsurrea.github.io/CV/profile.yaml
-// and projects are fetched from the GitHub API at build time.
-export const collections = {};
